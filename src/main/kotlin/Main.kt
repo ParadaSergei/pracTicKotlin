15bf93fb3d886131e82b0c36929e3a2994fadb73
NataliaPopova127 <pnatala127@mail.ru>
2022-10-04 16:22:48 +0300

    pr1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,67 @@
+fun main() {
+    val exercises: Int = 17   //#1 , 2 S
+    var exercisesSolved = 0
+    exercisesSolved+=1
+    println(exercisesSolved)
+
+    var skuns : Double = 17.0 //# 3 S
+    var ded :Double = 30.0
+    var srednii = 8.0
+    var asus :Double = ((skuns+ded)/srednii)
+    println(asus)
+
+
+    val testNumber = 50       //#4 S
+    val evenOdd = testNumber%2
+    println(evenOdd)
+
+    var answer = 0// 5 S
+    answer++
+    answer+=10
+    answer*=10
+    answer shr 3
+    println(answer)
+
+    var age: Int = 16 // 6 D
+    print(age)
+    age = 30
+    println(age)
+
+    val a: Double = 38.0 // 7 D
+    val b: Double = 15.0
+    val answer1 = (a*100)+b
+    val answer2 = (a*100) + (b*100)
+    val answer3 = (a*100) - (b*55)
+    println(answer1)
+    println(answer2)
+    println(answer3)
+
+    var anys = ((5+3) - ((4 / 2) * 2)) // 8 D
+    println(anys)
+
+
+    val average = (a+b)/ srednii //#9 D
+    println(average)
+
+   var fahrenheit:Double = 23.0
+    val celcius = fahrenheit
+    fahrenheit*1.8+32;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
